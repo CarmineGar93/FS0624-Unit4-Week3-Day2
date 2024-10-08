8d4683d1d1f7f37c985313aabf810119f7943cf8
Carmy93 <carmygargiulo45@gmail.com>
2024-10-08 14:26:15 +0200

    entity evento created

--- src/main/java/CarmineGargiulo/entities/Evento.java
+++ src/main/java/CarmineGargiulo/entities/Evento.java
@@ -1,15 +1,34 @@
 package CarmineGargiulo.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "events")
 public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
-    private String dataEvento;
+
+    @Column(name = "title" , nullable = false)
+    private String titolo;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDate dataEvento;
+
+    @Column(name = "description")
     private String descrizione;
+
+    @Column(name = "event_type", nullable = false)
+    @Enumerated(EnumType.STRING)
     private TipoEvento tipoEvento;
+
+    @Column(name = "max_participants")
     private int nrMaxPartecipanti;
 
-    public Evento(String name, String dataEvento, String descrizione, TipoEvento tipoEvento, int nrMaxPartecipanti) {
-        this.name = name;
+    public Evento(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int nrMaxPartecipanti) {
+        this.titolo = titolo;
         this.dataEvento = dataEvento;
         this.descrizione = descrizione;
         this.tipoEvento = tipoEvento;
@@ -24,19 +43,19 @@ public class Evento {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getTitolo() {
+        return titolo;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
     }
 
-    public String getDataEvento() {
+    public LocalDate getDataEvento() {
         return dataEvento;
     }
 
-    public void setDataEvento(String dataEvento) {
+    public void setDataEvento(LocalDate dataEvento) {
         this.dataEvento = dataEvento;
     }
 
@@ -68,8 +87,8 @@ public class Evento {
     public String toString() {
         return "Evento{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", dataEvento='" + dataEvento + '\'' +
+                ", name='" + titolo + '\'' +
+                ", dataEvento='" + dataEvento.toString() + '\'' +
                 ", descrizione='" + descrizione + '\'' +
                 ", tipoEvento=" + tipoEvento +
                 ", nrMaxPartecipanti=" + nrMaxPartecipanti +
